cc5712e5dee11f95b2dbc7b1c9fdc2d010f4f482
Akshith G <akshithg01@gmail.com>
2025-06-10 23:55:23 +0530

    count digits

--- warm-up/count-digits-in-number.js
+++ warm-up/count-digits-in-number.js
@@ -1,1 +1,19 @@
 // Write a program which counts the number of digits in a number
+function countDigits(n) {
+  n = Math.abs(n);
+  if (n == 0) {
+    return 1;
+  }
+  count = 0;
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count = count + 1;
+  }
+  return count;
+}
+
+console.log(countDigits(234));
+console.log(countDigits(234234));
+console.log(countDigits(-234));
+console.log(countDigits(1));
+console.log(countDigits(0));
