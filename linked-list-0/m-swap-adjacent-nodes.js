95438942eb64fd89b3beb5c1f548a798e885bd63
Akshith G <akshithg01@gmail.com>
2025-06-25 21:40:41 +0530

    swap-nodes

--- linked-list-0/m-swap-adjacent-nodes.js
+++ linked-list-0/m-swap-adjacent-nodes.js
@@ -1,7 +1,28 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
 function swapAdjacentNodes(head) {
   if (head === null || head.next === null) {
     return head;
   }
 
-  
+  let dummy = new Node(0);
+
+  let p = dummy;
+  let c = head;
+  let n = c.next;
+
+  while (c && c.next) {
+    c.next = n.next;
+    n.next = c;
+    p.next = n;
+    p = c;
+    c = p.next;
+    n = c && c.next;
+  }
+  return dummy.next;
 }
