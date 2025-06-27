562e9c37842c26170834e5c60e29d2ba15597429
Akshith G <akshithg01@gmail.com>
2025-06-27 13:07:17 +0530

    jewels-and-stones

---
+++ strings-0/e-jewels-and-stones.js
@@ -0,0 +1,15 @@
+// Time complexity - O(n)
+// Space complexity - O(52) or O(1), beacuse all jewels are alphabets and only 26*2 total alphabets english letters
+var numJewelsInStones = function (jewels, stones) {
+  let h = new Set();
+  for (let i = 0; i < jewels.length; i++) {
+    h.add(jewels[i]);
+  }
+  let c = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (h.has(stones[i])) {
+      c++;
+    }
+  }
+  return c;
+};
